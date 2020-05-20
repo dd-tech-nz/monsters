d542c828ff0e2a805d2b4e618810c4b8e196874e
dd-tech-nz <ddumolo@hotmail.com>
2020-05-20 14:43:10 +1200

    Updates

---
+++ precache-manifest.5ca085be887aa78cd47751f268a82238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e89ed6167cd25a4ec2d3c0c1d5e449e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "2cd54f43eaa207f46b7b",
+    "url": "/monsters/static/css/main.3201900a.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd54f43eaa207f46b7b",
+    "url": "/monsters/static/js/main.9f220bb1.chunk.js"
+  },
+  {
+    "revision": "77f8f01ea360998e236f",
+    "url": "/monsters/static/js/runtime-main.88ae1bce.js"
+  }
+]);
